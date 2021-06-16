79fe9a3aba436cd897db8bab3c4d60ac56a49a90
chieh <a22806869@hotmail.com.tw>
2021-06-16 15:34:21 +0800

    finished Dynamic value in Templates course, and make some easy dynamic vlaue on JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 
+
 function App() {
+
+  const title = "welcome to the new blog";
+  const likes = 50;
+  const link = "http://google.com"
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App component</h1>
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        <p>{"hello world"}</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
+        <p>{10}</p>
+        <p>{Math.random() * 10000}</p>
+        <a href={link}>Google site</a>
       </div>
     </div>
   );
