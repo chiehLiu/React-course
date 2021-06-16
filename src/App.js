badefc0faca71bd10804319deae8f5917fc81043
chieh <a22806869@hotmail.com.tw>
2021-06-16 16:00:16 +0800

    finished multiple components, and create few components and export those to the App.js for render purpose

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import './App.css';
-
+import Navbar from './Navbar'
+import Home from './Home'
 
 function App() {
-
-  const title = "welcome to the new blog";
-  const likes = 50;
-  const link = "http://google.com"
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        <p>{"hello world"}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{10}</p>
-        <p>{Math.random() * 10000}</p>
-        <a href={link}>Google site</a>
+        <Home />
       </div>
     </div>
   );
