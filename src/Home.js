1e2d28ca374e96787a49d50c131c2f90ab638aff
chieh <a22806869@hotmail.com.tw>
2021-06-20 16:13:49 +0800

    finished course useState, React dev tool, add useState on Home.js and use Dev tool to observe it

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,22 @@
+import { useState } from 'react';
+
 const Home = () => {
 
-    const handleClick = (e) => {
-        console.log(e.target.innerText);
+    // let name = 'Mario';
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(30);
+
+    const handleClick = () => {
+        setName('luigi')
+        setAge('23')
     }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} old</p>
             <button onClick={handleClick}>Click me</button>
         </div >
     );
 }
-
 export default Home;
