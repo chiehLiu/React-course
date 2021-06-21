2b07643ea49904dd71851267dc3573b5ca5f5e8a
chieh <a22806869@hotmail.com.tw>
2021-06-21 17:35:13 +0800

    finished props course, and pull the bloglist into a new component using props

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import BlogList from './BlogList'
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
@@ -7,14 +8,11 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
+    const title = "I am title"
+
     return (
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written By{blog.author}</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title={title} />
         </div >
     );
 }
