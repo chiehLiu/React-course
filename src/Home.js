03131fdf781634b84bc99bdc71d1efc61ed99750
chieh <a22806869@hotmail.com.tw>
2021-06-23 22:03:21 +0800

    finished handling fetch errors course, and made a state just for error message to render the page

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,8 @@ import BlogList from './BlogList'
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     const title = "I am title"
 
@@ -10,16 +12,27 @@ const Home = () => {
     // 通常 useEffect 可以用來 fetch 資料 或是 處理 authentication (驗證資訊)
     // 後方的空 array 可以讓 useEffect 只會在畫面 render 第一次的時候跑，其他時候都不會被觸發了
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        fetch('http://localhost:8000/blogss')
             .then(res => {
+                if (!res.ok) {
+                    throw Error('could not fetch the data from that resource');
+                }
                 return res.json()
             }).then((data) => {
-                setBlogs(data)
+                setBlogs(data);
+                setIsPending(false);
+                setError(null);
+            }).catch(err => {
+                setError(err.message);
+                setIsPending(false);
+                console.log(err.message);
             })
     }, [])
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title={title} />}
         </div >
     );
