fe7d587d76264741eba76bad7dff8790f1cf4437
chieh <a22806869@hotmail.com.tw>
2021-06-27 20:48:17 +0800

    finished course route parameters, and set useParams to the Link in this way we can go to the blog details through clicking the blog

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({ blogs, title }) => {
 
     return (
@@ -6,8 +8,11 @@ const BlogList = ({ blogs, title }) => {
             {
                 blogs.map((blog) => (
                     <div className="blog-preview" key={blog.id}>
-                        <h2>{blog.title}</h2>
-                        <p>Written By {blog.author}</p>
+                        <Link to={`/blogs/${blog.id}`}>
+                            <h2>{blog.title}</h2>
+                            <p>Written By {blog.author}</p>
+                        </Link>
+
                     </div>
                 ))
             }
